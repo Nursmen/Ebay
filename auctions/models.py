39124f0b85955863633172fd08383cbaf5490a77
Nursmen <nnursultan07@mail.ru>
2022-04-06 21:30:34 +0600

    fix close view

--- auctions/models.py
+++ auctions/models.py
@@ -14,7 +14,7 @@ class Listing(models.Model):
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(max_length=200, blank=True, null=True)
     is_active = models.BooleanField(default=True)
-    winner = models.ManyToManyField(User, blank=True, default=None, related_name='winner')
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='winner', blank=True, null=True)
 
     def __str__(self):
         return self.title
